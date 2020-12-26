d7a60b640133566bdf66046e5c8d8976099027a7
Olof Kastrup <olofkastrup@gmail.com>
2020-12-26 17:24:20 +0100

    Add gradle files and skeleton for spring boot

---
+++ src/main/java/com/kastrup/swedishverbs/SwedishverbsApplication.java
@@ -0,0 +1,13 @@
+package com.kastrup.swedishverbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwedishverbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwedishverbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kastrup/swedishverbs/SwedishverbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kastrup.swedishverbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwedishverbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
